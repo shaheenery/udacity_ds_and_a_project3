a7cf0c83835ea8f19e720c25f33360c292bd1ee4
shaheenery <mike.shaheen@gmail.com>
2019-09-08 00:02:03 -0700

    add solution for problem 6

---
+++ 6_unsorted_integer_array.py
@@ -0,0 +1,38 @@
+# Problem 6: Unsorted Integer Array
+# Max and Min in a Unsorted Array
+
+# In this problem, we will look for smallest and largest integer from a list of
+# unsorted integers. The code should run in O(n) time. Do not use Python's
+# inbuilt functions to find min and max.
+
+# Bonus Challenge: Is it possible to find the max and min in a single traversal?
+
+def get_min_max(ints):
+    """
+    Return a tuple(min, max) out of list of unsorted integers.
+
+    Args:
+       ints(list): list of integers containing one or more integers
+    """
+    min = None
+    max = None
+
+    for i in ints:
+        if min is None or i < min:
+            min = i
+        if max is None or i > max:
+            max = i
+
+    return (min, max)
+
+
+
+
+## Example Test Case of Ten Integers
+import random
+
+l = [i for i in range(0, 10)]  # a list containing 0 - 9
+random.shuffle(l)
+
+print ("Pass" if ((0, 9) == get_min_max(l)) else "Fail")
+# Sorting usually requires O(n log n) time Can you come up with a O(n) algorithm (i.e., linear time)?
